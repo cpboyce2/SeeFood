3354edb7c62c5bdabb124d0bdd8f89c2361a12ce
connor_boyce <cpboycercf@gmail.com>
2019-09-13 15:22:41 -0400

    Implemented the InceptionV3 model for visual recognition

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -25,8 +25,36 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
                  imageView.image = userPickedImage
+            guard let coreImage = CIImage(image: userPickedImage) else{
+                fatalError("Could not convert to a CI Image")
+            }
+            detect(image: coreImage)
         }
         imagePicker.dismiss(animated: true, completion: nil)
+
+    }
+
+    func detect(image: CIImage){
+        
+        // VNCoreMLModel comes from the Vision library
+        guard let V3Model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading COREML model failed")
+        }
+        let request = VNCoreMLRequest(model: V3Model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("There was an error making the request")
+            }
+            print(results)
+        }
+        // The handler specifies what image you want to classify
+
+        let handler = VNImageRequestHandler(ciImage: image)
+
+        do{
+             try handler.perform([request])
+        } catch {
+            print(error)
+        }
 
     }
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
